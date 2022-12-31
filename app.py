8149ba2c6566d1a9730ce53122a57661c36fe0bc
mahamedhat <93611364+mahamedhat@users.noreply.github.com>
2022-12-31 03:56:49 +0200

    fix zero button

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from flask import Flask
+import numpy as np
+import pandas as pd
+from scipy import signal
+from flask import (Flask, json, jsonify, redirect, render_template, request,
+                   url_for)
 
 app = Flask(__name__)
 
-
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     
